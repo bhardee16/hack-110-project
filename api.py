b343e504d7040ef34f81f56dc3de173741350048
Blake Hardee <bhardee@unc.edu>
2021-11-13 01:13:30 -0500

    7

--- api.py
+++ api.py
@@ -5,4 +5,4 @@ from app import parks
 
 def get_park_activities(park: str) -> dict[str, str]:
     park_activities: Response = get("https://developer.nps.gov/api/v1/parks", params={"api_key": "yciKZcC0PxNgfw0ArNOH4kwrghRj33jsJF7znKsn"})
-# bruh
+# bru
